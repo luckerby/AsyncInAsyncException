fd3c04f1c95a9e59d8b3300190cbc0f804d48b43
Mihai Albert <mihai.albert@gmail.cm>
2021-05-09 09:17:54 +0300

    Add project files.

---
+++ AsyncInAsyncException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncInAsyncException
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new AsyncInAsyncException().CallerMethod();
+        }
+    }
+
+    class AsyncInAsyncException
+    {
+        public async Task CallerMethod()
+        {
+            await CalleeMethod();
+        }
+
+        public async Task CalleeMethod()
+        {
+            throw (new NotImplementedException());
+        }
+    }
+
+}
